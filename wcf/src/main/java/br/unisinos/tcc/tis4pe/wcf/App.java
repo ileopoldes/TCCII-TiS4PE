ecc6b9bc557b70eaabcfad2442f4de9c478df437
Paulo <leopoldes@gmail.com>
2015-03-28 15:02:26 -0300

    Configuração básica do projeto ok

---
+++ wcf/src/main/java/br/unisinos/tcc/tis4pe/wcf/App.java
@@ -0,0 +1,13 @@
+package br.unisinos.tcc.tis4pe.wcf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
