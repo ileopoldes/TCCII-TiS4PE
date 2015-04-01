aa1af81329d77b099a0db0440c833400c1d9bc98
Paulo <leopoldes@gmail.com>
2015-04-01 00:25:40 -0300

    Trabalhando na regex adequada para o padrão de texto do exemplo

--- wcf/src/main/java/br/unisinos/tcc/tis4pe/wcf/App.java
+++ wcf/src/main/java/br/unisinos/tcc/tis4pe/wcf/App.java
@@ -1,13 +1,20 @@
 package br.unisinos.tcc.tis4pe.wcf;
 
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "TiS4PE!\n\n" );
+        InputStreamHandler in = new InputStreamHandler();
+        
+        String pathFile = "/home/ileopoldes/tmp/ClarkNet.txt";
+        
+        in.readStream(pathFile);
     }
 }
--- wcf/src/main/java/br/unisinos/tcc/tis4pe/wcf/InputStreamHandler.java
+++ wcf/src/main/java/br/unisinos/tcc/tis4pe/wcf/InputStreamHandler.java
@@ -3,6 +3,53 @@
  */
 package br.unisinos.tcc.tis4pe.wcf;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class InputStreamHandler {
 
+	/*
+	 * lê arquivo
+	 * http://ita.ee.lbl.gov/html/contrib/ClarkNet-HTTP.html
+	 */
+	@SuppressWarnings("resource")
+	public void readStream(String fileName) throws IOException {
+		Scanner scanner;
+		try {
+			String data = "[ffff234:09 1/23/23]  [gggg]";  
+		   //String regex = "([\\.\\w\\d\\s])+";
+			String regex = "([.*])+";
+		    Pattern p = Pattern.compile(regex);  
+		    Matcher m = p.matcher(data);  
+		    while (m.find()) {  
+		        System.out.println(m.group());  
+		    }  
+		    
+			Pattern po = Pattern.compile(regex);
+			FileReader reader = new FileReader(fileName);
+			scanner = new Scanner(reader)
+					.useDelimiter(po);
+			// tagoss.clark.net - - [04/Sep/1995:00:00:27 -0400] "GET / HTTP/1.0" 200 1834
+			int i = 0;
+			while (scanner.hasNext()) {
+				String primeiraParte = scanner.next();
+				String meio = scanner.next();
+				String segundaParte = scanner.next();
+				//System.out.println(primeiraParte);
+				System.out.println(meio);
+				//System.out.println(segundaParte);
+				i++;
+				if(i==5) break;
+			}
+			reader.close();
+			scanner.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
